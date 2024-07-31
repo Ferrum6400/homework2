d3cb8dce8fa9679f38163c4c465123d8a3e4bd1d
Ferrum6400 <revolted6400@mail.ru>
2024-07-31 15:30:22 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+number_of_task = 12
+number_of_hours_spent = 1.5
+Course_name = 'Pythone'
+Time_for_one_task = 0.125
+print("Курс:", Course_name, "всего задач:", number_of_task, "затрачено часов:", number_of_hours_spent, "среднее время выполнения", Time_for_one_task, "часа")
